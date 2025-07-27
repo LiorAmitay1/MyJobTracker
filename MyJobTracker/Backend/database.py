8859dd664bc2aa514f20ad428bd86d4aa807fb1d
LiorAmitay1 <lioramit@post.bgu.ac.il>
2025-07-27 14:36:17 +0300

    Deleting irrelevant comments

--- MyJobTracker/Backend/database.py
+++ MyJobTracker/Backend/database.py
@@ -4,19 +4,15 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 DATABASE_URL = "mysql+pymysql://root:Lior!209470236@127.0.0.1:3306/jobtracker"
 
 
-# יצירת מנוע שמחבר את Python למסד הנתונים
 engine = create_engine(DATABASE_URL)
 
-# הגדרה איך לייצר "session" (חיבור זמני לבסיס הנתונים)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# בסיס לבניית מודלים (טבלאות)
 Base = declarative_base()
 
-# פונקציה שתייצר חיבור חדש לבסיס הנתונים עבור כל בקשת API
 def get_db():
-    db = SessionLocal()       # לפתוח חיבור זמני
+    db = SessionLocal()
     try:
-        yield db              # להעביר אותו הלאה
+        yield db
     finally:
-        db.close()            # לסגור את החיבור בסוף
+        db.close()
